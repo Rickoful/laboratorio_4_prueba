92000d8535fc86dfb1e639784aa889b4c5255adb
Mauricio Cecilio Martinez Cornejo <mauriciowaso@gmail.com>
2026-03-16 11:20:51 -0400

    nuevo en rama-prueba

--- ParImpar.java
+++ ParImpar.java
@@ -4,7 +4,7 @@ public class ParImpar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Ingresa un número: ");
+        System.out.print("intro. numero: ");
         int numero = scanner.nextInt();
         
         if (numero % 2 == 0) {
